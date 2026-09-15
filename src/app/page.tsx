5e1166e2e844ce49d735907d1f50931efab184aa
mikiyas37 <mikiyassolomonw@gmail.com>
2026-09-15 23:51:16 +0300

    re-deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth";
-
 export default async function Home() {
   const session = await getSession();
   redirect(session ? "/dashboard" : "/login");
